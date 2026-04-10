3dad2052b755aba1306835f9665b31d0dbbf370d
fozilovyosin2010 <umnovanika1@gmail.com>
2026-04-10 17:23:28 +0500

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "./Store/Store";
+import { delData } from "./Reducers/userSlice";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 
 const App = () => {
-  return <div>App</div>;
+  const { data } = useSelector((store: RootState) => store.users);
+
+  const dispatch = useDispatch();
+  console.log(data);
+
+  function handleClickDel(id: number) {
+    dispatch(delData(id));
+  }
+
+  const [openAdd, setOpenSAdd] = useState(false);
+  function hanClickOpenAdd() {}
+
+  return (
+    <div className="p-[10px_20px]">
+      <header className="py-2">
+        <button onClick={hanClickOpenAdd} className="bg-blue-500 text-[#fff]">
+          Add
+        </button>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((e) => {
+            return (
+              <tr>
+                <td>{e.name}</td>
+                <td>{e.status ? "ACTIVE" : "INACTIVE"}</td>
+                <td>
+                  <div>
+                    <button
+                      onClick={() => handleClickDel(e.id)}
+                      className="border border-[red]"
+                    >
+                      Del
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Reducers/userSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+export interface Idata {
+  id: number;
+  name: string;
+  status: boolean;
+}
+export interface UserStates {
+  data: Idata[];
+}
+const initialState: UserStates = {
+  data: [
+    { id: 1, name: "Yosin", status: false },
+    { id: 2, name: "Jordan", status: true },
+  ],
+};
+
+export const userSlice = createSlice({
+  name: "user",
+
+  initialState,
+
+  reducers: {
+    delData: (state, action) => {
+      state.data = state.data.filter((e) => e.id != action.payload);
+    },
+
+    addData: (state, action) => {
+      state.data = [...state.data, action.payload];
+    },
+  },
+});
+
+export const { delData } = userSlice.actions;
+
+export default userSlice.reducer;
