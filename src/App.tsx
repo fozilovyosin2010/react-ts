a5ad649b1a5c95e30b55f840b586116ccdb1e94a
fozilovyosin2010 <umnovanika1@gmail.com>
2026-04-27 16:52:58 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./Components/Counter";
+import { ErrorBoundary } from "react-error-boundary";
+
+const Compos = () => {
+  return <div>Some error</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ErrorBoundary FallbackComponent={Compos}>
+        {/* ErrorBoundary can handle Rendering & lifecycle errors */}
+        <Counter />
+      </ErrorBoundary>
+    </div>
+  );
 };
 
 export default App;
